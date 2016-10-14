eeb7a7849f5c399b67e1972d991afa93a2ff216b
Dewei Chen <deweichen47@gmail.com>
2016-10-13 23:04:33 -0400

    fixed some arg parse bugs

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const Master = require("./master/master").Master;
 const Worker = require("./worker/worker").Worker;
 
 const argv = require('yargs')
-  .usage('$0 [options] <master|worker> master_host:port [worker_host:port]')
-  .example('$0 -m  localhost:5050 -w localhost:5051 worker')
-  .example('$0 -m localhost:5050 master')
+  .usage('mr-node [options] <master|worker> master_host:port [worker_host:port]')
+  .example('mr-node worker localhost:5050 localhost:5051')
+  .example('mr-node master localhost:5050')
   .number('m')
   .number('n')
   .default('m', 5)
   .default('n', 3)
   .alias('m', 'nmap')
   .alias('n', 'nreduce')
+  .describe('m', 'number of map jobs')
+  .describe('n', 'number of reduce jobs')
+  .help('h')
   .argv
 
 function validateArgs(callback) {
@@ -23,16 +26,13 @@ function validateArgs(callback) {
     return callback(new Error("Argument must be either 'master' or 'worker'"));
   }
   if (serviceType === 'master') { 
-    if (!argv.m) {
-      return callback(new Error("Master service must provide valid m options"));
+    if (!argv.m || !argv.n) {
+      return callback(new Error("Master service must provide valid m and n options"));
     }
     if (!masterAddr) {
       return callback(new Error("Missing master host address"));
     }
   } else if (serviceType === 'worker') {
-    if (!argv.m || !argv.n) {
-      return callback(new Error("Worker service must provide valid m and n options"));
-    }
     if (!masterAddr || !workerAddr) {
       return callback(new Error("Missing master and/or worker host address(es)"));
     }
@@ -53,12 +53,9 @@ function main() {
       console.error(err);
       process.exit(-1);
     }
-    let service;
-    if (args.serviceType === 'master') {
-      service = new Master(args.masterAddr, args.numMapJobs, args.numReduceJobs);
-    } else {
-      service = new Worker(args.workerAddr, args.masterAddr)
-    }
+    const service = args.serviceType === 'master' ? 
+      new Master(args.masterAddr, args.numMapJobs, args.numReduceJobs) :
+      new Worker(args.workerAddr, args.masterAddr)
     service.start();
   });
 }
